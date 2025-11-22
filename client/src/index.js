3b5eb38fca6af357e39bcf7eca6f4cfcce563fc6
Mahir <mahir.patel.cg@gmail.com>
2025-11-22 16:44:13 +0530

    added render link

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import axios from 'axios';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+
+axios.defaults.baseURL = 'https://odoo-spit-1.onrender.com';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,4 +14,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
